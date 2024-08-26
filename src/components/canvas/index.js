9727802a3d3138a9e8e6fbc5a0bb32c55c6f6ddf
Tawfik <tawfy@Machine001.local>
2024-08-26 14:42:19 +0530

    Updates

--- src/components/canvas/index.js
+++
@@ -1,8 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import StarsCanvas from "./Stars";
-import ComputersCanvas from "./Computers";
-import AncientCanvas from "./Ancient";
-
-
-export { EarthCanvas, ComputersCanvas, BallCanvas, StarsCanvas, AncientCanvas };
